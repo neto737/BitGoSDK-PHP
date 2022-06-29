508122c24a1149f815931888f9024a0194592f50
Neto <tynhomello@gmail.com>
2022-06-29 11:02:03 -0300

    Added tests for client

---
+++ tests/clientTest.php
@@ -0,0 +1,46 @@
+<?php
+
+use neto737\BitGoSDK\Authentication\Authentication;
+use neto737\BitGoSDK\Authentication\Environment;
+use neto737\BitGoSDK\Client;
+use neto737\BitGoSDK\Enum\CurrencyCode;
+use neto737\BitGoSDK\Enum\Environments;
+use neto737\BitGoSDK\Resources\Wallet\Address;
+use PHPUnit\Framework\TestCase;
+
+class clientTest extends TestCase {
+
+    public function testAuthentication(): void {
+        $auth = new Authentication('YOUR_API_KEY');
+
+        $this->assertSame('YOUR_API_KEY', $auth->getApiKey());
+
+        $auth->setApiKey('YOUR_SECOND_API_KEY');
+
+        $this->assertSame('YOUR_SECOND_API_KEY', $auth->getApiKey());
+    }
+
+    public function testEnvironment(): void {
+        $env = new Environment(Environments::MAINNET, '59cd72485007a239fb00282ed480da1f', CurrencyCode::BTC);
+
+        $this->assertSame('https://app.bitgo.com/api/v2/', $env->getEnvironment());
+        $this->assertSame('59cd72485007a239fb00282ed480da1f', $env->getWalletId());
+        $this->assertSame('BTC', $env->getCurrency());
+    }
+
+    public function testEnvironmentInvalidWalletId(): void {
+        $this->expectExceptionMessage('Invalid wallet id format.');
+
+        $env = new Environment(Environments::MAINNET, 'YOUR_WALLET_ID', CurrencyCode::BTC);
+    }
+
+    public function testAddress(): void {
+        $auth = new Authentication('YOUR_API_KEY');
+        $env = new Environment(Environments::TESTNET, '59cd72485007a239fb00282ed480da1f', CurrencyCode::BTC);
+
+        $client = new Client($auth, $env);
+        $address = $client->Address();
+        
+        $this->assertEquals(new Address($client), $address);
+    }
+}
